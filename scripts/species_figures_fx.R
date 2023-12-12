078c3d0487df597a466476a7d8f45ce04375a5f2
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2023-12-12 16:35:13 -0300

    Scenario and stauts trends plots per species done

--- scripts/species_figures_fx.R
+++ scripts/species_figures_fx.R
@@ -2,10 +2,17 @@
 
 # Function to create line figure of biomass trend per species
 
-spp_trend_fig <- function(data,taxon_key){
+spp_trend_scen_fig <- function(data,taxon,data_path){
   
+  # Global variables
+  taxon_name <- spp_list %>% filter(taxon_key %in% taxon) %>% pull(common_name)
   
-  data %>% 
+  # Verbantim
+  print(paste("Creating plot for",taxon_name,"(",taxon,")"))
+  
+  # Analysis
+  fig <- data %>%
+    filter(taxon_key %in% taxon) %>%
     group_by(taxon_key,year,variable,ssp,scen) %>% 
     summarise(
       total_value = sum(mean_value,na.rm = T),
@@ -14,42 +21,158 @@ spp_trend_fig <- function(data,taxon_key){
     mutate(
       period = ifelse(year %in% seq(1995,2014,1),"history",year)
     ) %>% 
-    filter(!is.na(period)) %>% 
+    filter(period %in% c("history",seq(2015,2100,1))) %>% 
     group_by(variable,scen,ssp,period) %>% 
     summarise(
       mean_variable = mean(total_value, na.rm = T),
       .groups = "drop"
     ) %>% 
-    group_by(index,variable,scen,ssp,lat,lon) %>% 
-    mutate(ratio = (lag(mean_variable)-mean_variable)/mean_variable) %>%
-    # mutate(ratio = mean_variable/lag(mean_variable)) %>% 
-    filter(!is.na(ratio)) %>% 
+    group_by(variable,scen,ssp) %>% 
+    mutate(
+      relative_mean = (mean_variable - mean_variable[period == "history"])/abs(mean_variable[period == "history"])*100,
+      running_mean = zoo::rollapply(relative_mean, width = 10, FUN = mean, align = "right", fill = NA),
+      year = ifelse(period == "history",2014,period) %>% as.numeric(.),
+      facet = ifelse(variable == "Abd","Abundance","Maximum Catch Potential"),
+      SSP = ifelse(ssp == 26,"126","585"),
+      Scenario = scen
+    ) %>% 
+      # View()
     ggplot() +
-    geom_tile(
-      # metR::geom_contour_fill(
+    geom_line(
       aes(
-        x = lon,
-        y = lat,
-        # z = ratio,
-        fill = ratio
-      )#,
-      # bins = 5
+        x = year,
+        y = running_mean,
+        color = Scenario,
+        linetype = SSP
+      ),
+      alpha = 0.5
     ) +
-    facet_grid(ssp+variable ~ scen) +
-    scale_fill_gradient2() +
-    MyFunctions::my_land_map() +
-    geom_sf(data = ammb_sf, aes(), fill = "transparent") +
-    coord_sf(
-      xlim = c(-93,-83),
-      ylim = c(0,10)
-    )
+    geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
+    labs(x = "Year",
+         y = "Relative Change (%)") +
+    facet_grid(~ facet) +
+    ggtheme_p(
+      leg_pos = "right"
+    ) +
+    scale_x_continuous(limits = c(2020,2100)) +
+      scale_color_manual(values = scenario_pallet)
   
+  fig_name <- paste0(data_path,"results/figures/scenario_trend/",taxon_name %>% str_replace(" ", "_"),"_scen_trend.png")
   
-  fig_name <- paste0(taxon)
-  
-  ggsave(filename = "../results/abd_catch_scen_lin.png", 
+  ggsave(filename = fig_name, 
          plot = fig,
-         width = 14,
-         height = 10)
+         width = 10,
+         height = 5)
 }
+
+
+# Function to create line figure of biomass trend per species
+
+spp_trend_status_fig <- function(data,taxon,data_path){
+  
+  # Global variables
+  taxon_name <- spp_list %>% filter(taxon_key %in% taxon) %>% pull(common_name)
+  
+  # Verbantim
+  print(paste("Creating plot for",taxon_name,"(",taxon,")"))
+  
+  # Analysis
+  fig <-
+    data %>%
+    filter(taxon_key %in% taxon) %>%
+    group_by(taxon_key,year,variable,ssp,scen,status) %>% 
+    summarise(
+      total_value = sum(mean_value,na.rm = T),
+      .groups = "drop"
+    ) %>% 
+    mutate(
+      period = ifelse(year %in% seq(1995,2014,1),"history",year)
+    ) %>% 
+    filter(period %in% c("history",seq(2015,2100,1))) %>% 
+    group_by(variable,scen,status,ssp,period) %>% 
+    summarise(
+      mean_variable = mean(total_value, na.rm = T),
+      .groups = "drop"
+    ) %>% 
+    group_by(variable,scen,status,ssp) %>% 
+    mutate(
+      relative_mean = (mean_variable - mean_variable[period == "history"])/abs(mean_variable[period == "history"])*100,
+      running_mean = zoo::rollapply(relative_mean, width = 10, FUN = mean, align = "right", fill = NA),
+      year = ifelse(period == "history",2014,period) %>% as.numeric(.),
+      facet = ifelse(variable == "Abd","Abundance","Maximum Catch Potential"),
+      SSP = ifelse(ssp == 26,"126","585"),
+      Scenario = scen,
+      Status = status
+    ) %>% 
+  ggplot() +
+    geom_line(
+      aes(
+        x = year,
+        y = running_mean,
+        color = Status,
+        linetype = SSP
+      ),
+      alpha = 0.5
+    ) +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
+    labs(x = "Year",
+         y = "Relative Change (%)") +
+    facet_grid(Scenario ~ facet) +
+    ggtheme_p(
+      leg_pos = "right"
+    ) +
+    scale_x_continuous(limits = c(2020,2100)) +
+      scale_color_manual(values = status_pallet)
+  
+  fig_name <- paste0(data_path,"results/figures/status_trend/",taxon_name %>% str_replace(" ", "_"),"_status_trend.png")
+  
+  ggsave(filename = fig_name, 
+         plot = fig,
+         width = 10,
+         height = 7)
+}
+
+
+
+
+
+# Themes
+
+ggtheme_p <- function(ax_tx_s = 12,
+                      axx_tx_ang = 0,
+                      axy_tx_ang = 0,
+                      ax_tl_s = 14, 
+                      leg_pos = "top", 
+                      leg_aline = 0.5, 
+                      leg_tl_s = 14,
+                      leg_tx_s = 12,
+                      hjust = 0, 
+                      facet_tx_s = 12) {
+  theme(
+    panel.background = element_blank(),
+    strip.background = element_blank(),
+    panel.border   = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(size = ax_tx_s,
+                               angle = axx_tx_ang,
+                               hjust = hjust,
+                               face = "plain",
+                               color = "black"),
+    axis.text.y = element_text(size = ax_tx_s,
+                               color = "black"),
+    axis.title = element_text(size = ax_tl_s),
+    # For legend
+    legend.key = element_rect(colour = NA, fill = NA),
+    legend.position = leg_pos,
+    legend.title.align = leg_aline,
+    legend.title = element_text(size = leg_tl_s),
+    legend.text = element_text(size = leg_tx_s),
+    # For `facet_wrap`
+    strip.text = element_text(size = facet_tx_s, colour = "black")
+  )
+}
+
 
