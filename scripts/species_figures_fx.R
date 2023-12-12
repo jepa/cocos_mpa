76e5702ff2f9b03826d799229c6573d8ae1159e6
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2023-12-12 13:32:04 -0300

    figure trend

--- scripts/species_figures_fx.R
+++ scripts/species_figures_fx.R
@@ -2,25 +2,47 @@
 
 # Function to create line figure of biomass trend per species
 
-spp_trend_fig <- function(data,taxon){
+spp_trend_fig <- function(data,taxon_key){
   
-  fig <- data %>% 
-    filter(takon_key %in% taxon) %>% 
-    group_by(takon_key,year,scen,status,ssp,variable) %>% 
+  
+  data %>% 
+    group_by(taxon_key,year,variable,ssp,scen) %>% 
     summarise(
-      total_value = sum(mean_value,na.rm = T)
+      total_value = sum(mean_value,na.rm = T),
+      .groups = "drop"
     ) %>% 
-    # filter(scen == "S1") %>% 
+    mutate(
+      period = ifelse(year %in% seq(1995,2014,1),"history",year)
+    ) %>% 
+    filter(!is.na(period)) %>% 
+    group_by(variable,scen,ssp,period) %>% 
+    summarise(
+      mean_variable = mean(total_value, na.rm = T),
+      .groups = "drop"
+    ) %>% 
+    group_by(index,variable,scen,ssp,lat,lon) %>% 
+    mutate(ratio = (lag(mean_variable)-mean_variable)/mean_variable) %>%
+    # mutate(ratio = mean_variable/lag(mean_variable)) %>% 
+    filter(!is.na(ratio)) %>% 
     ggplot() +
-    geom_line(
+    geom_tile(
+      # metR::geom_contour_fill(
       aes(
-        x = as.numeric(year),
-        y = total_value,
-        color = scen
-      )
+        x = lon,
+        y = lat,
+        # z = ratio,
+        fill = ratio
+      )#,
+      # bins = 5
     ) +
-    facet_wrap(ssp+variable~status,
-               scales = "free")
+    facet_grid(ssp+variable ~ scen) +
+    scale_fill_gradient2() +
+    MyFunctions::my_land_map() +
+    geom_sf(data = ammb_sf, aes(), fill = "transparent") +
+    coord_sf(
+      xlim = c(-93,-83),
+      ylim = c(0,10)
+    )
   
   
   fig_name <- paste0(taxon)
